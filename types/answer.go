ce14145ae1b5afbd48c61b8adc35a5fd658b4344
Suryana <nanasuryana335@gmail.com>
2023-12-10 05:40:05 +0700

    refactor(types/answer): change json naming into [field]ID

--- types/answer.go
+++ types/answer.go
@@ -8,8 +8,8 @@ import (
 
 type Answer struct {
 	ID primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	UserID primitive.ObjectID `bson:"user_id" json:"user_id"`
-	QuestionID primitive.ObjectID `bson:"question_id" json:"question_id"`
+	UserID primitive.ObjectID `bson:"userID" json:"userID"`
+	QuestionID primitive.ObjectID `bson:"questionID" json:"questionID"`
 	Content string `bson:"content" json:"content"`
 	Upvotes []primitive.ObjectID `bson:"upvotes,omitempty" json:"upvotes,omitempty"`
 	Downvotes []primitive.ObjectID `bson:"downvotes" json:"downvotes"`
@@ -17,7 +17,7 @@ type Answer struct {
 }
 
 type CreateAnswerParams struct {
-	UserID primitive.ObjectID `json:"user_id"`
-	QuestionID primitive.ObjectID `json:"question_id"`
+	UserID primitive.ObjectID `json:"userID"`
+	QuestionID primitive.ObjectID `json:"questionID"`
 	Content string `json:"content"`
 }
