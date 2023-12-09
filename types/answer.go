11f51fc59bc91d8228c5bfb8c024e04707565f2e
Suryana <nanasuryana335@gmail.com>
2023-12-10 05:13:51 +0700

    feat(types/answer): create new type => Answer

--- types/answer.go
+++ types/answer.go
@@ -1,7 +1,23 @@
 package types
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Answer struct {
 	ID primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserID primitive.ObjectID `bson:"user_id" json:"user_id"`
+	QuestionID primitive.ObjectID `bson:"question_id" json:"question_id"`
+	Content string `bson:"content" json:"content"`
+	Upvotes []primitive.ObjectID `bson:"upvotes,omitempty" json:"upvotes,omitempty"`
+	Downvotes []primitive.ObjectID `bson:"downvotes" json:"downvotes"`
+	CreatedAt time.Time `bson:"createdAt" json:"createdAt"`
+}
+
+type CreateAnswerParams struct {
+	UserID primitive.ObjectID `json:"user_id"`
+	QuestionID primitive.ObjectID `json:"question_id"`
+	Content string `json:"content"`
 }
