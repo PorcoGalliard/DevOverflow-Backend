fddabe76087be05efa7f1737a9dcc3c2ae27c571
Suryana <nanasuryana335@gmail.com>
2023-12-10 05:17:22 +0700

    feat(db/answer_store): implement answer store and CreateAnswer logic

--- db/answer_store.go
+++ db/answer_store.go
@@ -1,1 +1,40 @@
 package db
+
+import (
+	"context"
+	"os"
+
+	"github.com/fullstack/dev-overflow/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const ANSWERCOLL = "answers"
+
+type AnswerStore interface {
+	CreateAnswer(context.Context, *types.Answer) (*types.Answer,error)
+}
+
+type MongoAnswerStore struct {
+	client *mongo.Client
+	coll *mongo.Collection
+}
+
+func NewMongoAnswerStore(client *mongo.Client) *MongoAnswerStore {
+	var mongoenvdbname = os.Getenv("MONGO_DB_NAME")
+	return &MongoAnswerStore{
+		client: client,
+		coll: client.Database(mongoenvdbname).Collection(ANSWERCOLL),
+	}
+}
+
+func (s *MongoAnswerStore) CreateAnswer(ctx context.Context, answer *types.Answer) ( *types.Answer, error) {
+	res, err := s.coll.InsertOne(ctx, answer)
+	if err != nil {
+		return nil, err
+	}
+	
+	answer.ID = res.InsertedID.(primitive.ObjectID)
+
+	return answer, nil
+}
