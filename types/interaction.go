35fdf7dcd235903cdb50a0933b414575e5fd64cf
Suryana <nanasuryana335@gmail.com>
2023-12-13 10:11:13 +0700

    refactor(types/interaction): refactor interaction type structure

--- types/interaction.go
+++ types/interaction.go
@@ -5,12 +5,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Interaction struct {
 	ID primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	UserID primitive.ObjectID `bson:"userID" json:"userID"`
-	User *User `bson:"user,omitempty" json:"user,omitempty"`
 	Action string `bson:"action" json:"action"`
 	QuestionID primitive.ObjectID `bson:"questionID" json:"questionID"`
 	AnswerID primitive.ObjectID `bson:"answerID" json:"answerID"`
 	Tags []primitive.ObjectID `bson:"tags" json:"tags"`
-	TagsDetails []*Tag `bson:"tagsDetails,omitempty" json:"tagsDetails,omitempty"`
 	CreatedAt int64 `bson:"createdAt" json:"createdAt"`
 }
 
